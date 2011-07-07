505e9d3e2c7daf07c2e5eebe29afb4878041e91e
Maik R��der <maikroeder@gmail.com>
2011-07-07 13:54:02 +0000

    add

---
+++ raisin/restyler/tests/test_page.py
@@ -0,0 +1,28 @@
+import sys
+import unittest
+from raisin.restyler import page
+
+class ResourceTest(unittest.TestCase):    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        
+    def test_page(self):
+        self.failUnless(DBS[None] == self.dummy_db)
+
+# make the test suite.
+def suite():
+    loader = unittest.TestLoader()
+    testsuite = loader.loadTestsFromTestCase(ResourceTest)
+    return testsuite
+
+# Make the test suite; run the tests.
+def test_main():
+    testsuite = suite()
+    runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
+    result = runner.run(testsuite)
+
+if __name__ == "__main__":
+    test_main()
