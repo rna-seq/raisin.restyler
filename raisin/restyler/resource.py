104b0704fdd126478278b7610ace66108a5ae46a
Maik R��der <maikroeder@gmail.com>
2011-08-26 22:28:30 +0000

    =pylint

--- raisin/restyler/resource.py
+++ raisin/restyler/resource.py
@@ -7,17 +7,23 @@ from config import PICKLED
 
 
 class Resource:
+    """Fetch RESTful resource using a provider implmenting the method: 
+    def get(uri, content_type)
+    """
 
     def __init__(self, provider):
         """Store the provider used to fetch the resources."""
         self.provider = provider
 
-    def get(self, name, content_type=PICKLED, kwargs={}):
+    def get(self, name, content_type=PICKLED, kwargs=None):
         """Get a resource from a resource provider"""
         try:
-            uri = RESOURCES[name]['uri'] % kwargs
+            resource = RESOURCES[name]
         except KeyError:
             raise
+        uri = resource['uri']
+        if not kwargs is None:
+            uri = uri % kwargs
         result = self.provider.get(uri, content_type)
         if not result is None and content_type == PICKLED:
             result = pickle.loads(result)
