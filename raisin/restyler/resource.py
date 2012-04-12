37c11522b9654011b1120eae7e861e596415f5a7
Maik R��der <maikroeder@gmail.com>
2012-04-12 14:31:31 +0000

    =Catch the NoMoreData exception of http_parser

--- raisin/restyler/resource.py
+++ raisin/restyler/resource.py
@@ -4,7 +4,7 @@ by resource name.
 import pickle
 from raisin.box import RESOURCES
 from config import PICKLED
-
+from http_parser.http import NoMoreData
 
 class Resource:
     """Fetch RESTful resource using a provider implementing the method:
@@ -24,7 +24,16 @@ class Resource:
         uri = resource['uri']
         if not kwargs is None:
             uri = uri % kwargs
-        result = self.provider.get(uri, content_type)
+        result = None
+        try:
+            result = self.provider.get(uri, content_type)
+        except NoMoreData:
+            # Catch the following exception:
+            # http_parser-0.7.5-py2.6-linux-x86_64.egg/http_parser/http.py'
+            # line 70 in _check_headers_complete
+            # raise NoMoreData("Can't parse headers")
+            # NoMoreData: Can't parse headers
+            pass 
         if not result is None and content_type == PICKLED:
             result = pickle.loads(result)
         return result
