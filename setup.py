2c311a96720296c2b14a83413b4c3edb63cf7030
Maik R��der <maikroeder@gmail.com>
2012-07-20 10:41:04 +0000

    =preparing 1.1.2

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.1.1'
+version = '1.1.2'
+long_description = """The raisin.restyler package is a part of Raisin, the web application
+used for publishing the summary statistics of Grape, a pipeline used for processing and
+analyzing RNA-Seq data."""
 
 setup(name='raisin.restyler',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      description="A package used in the Raisin web application",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Operating System :: POSIX :: Linux'],
+      keywords='RNA-Seq pipeline ngs transcriptome bioinformatics ETL',
+      author='Maik Roder',
+      author_email='maikroeder@gmail.com',
+      url='http://big.crg.cat/services/grape',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       namespace_packages = ['raisin'],
       package_data = {'raisin.restyler':['templates/*.pt']},
