2ecdb1f76f869dc3cd31726d2763e3f8622256b0
Maik Röder <maikroeder@gmail.com>
2012-11-11 00:55:08 +0100

    =fix unit tests

--- raisin/restyler/tests/test_page.py
+++ raisin/restyler/tests/test_page.py
@@ -54,12 +54,12 @@ class ResourceTest(unittest.TestCase):
         p = page.Page(request)
         bcr = [{'url': 'http://example.com/',
                 'title': 'Projects'},
-               {'url': 'http://example.com/project/ENCODE/tab/experiments/',
+               {'url': 'http://example.com/project/ENCODE',
                 'title': 'Project: ENCODE'},
-               {'url': 'http://example.com/project/ENCODE/None/None/tab/None',
+               {'url': 'http://example.com/project/ENCODE/None/None',
                 'title': 'Experiment: None'}]
         print p.get_breadcrumbs(request)
-        self.failUnless(p.get_breadcrumbs(request) == bcr)
+        self.failUnless(p.get_breadcrumbs(request) == bcr, p.get_breadcrumbs(request))
 
 
 # make the test suite.
